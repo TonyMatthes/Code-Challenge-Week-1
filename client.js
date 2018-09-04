00109aa72c32cf9283bf9c5e6e5723ccb0b72d72
Tony Matthes <tony@Tonys-MacBook-Pro.local>
2018-09-04 13:28:01 -0500

    added counter

--- client.js
+++ client.js
@@ -1,9 +1,14 @@
+let counter = 0
+
 $(document).ready(onReady);
 
 function onReady(){
-    $('#createButton').on('click', addDiv)
+    $('#createButton').on('click', addDiv);
+    
 }
 
 function addDiv(){
+    counter++;
+    console.log(counter);
     $('body').append('<div class=colorBox></div');
 }
