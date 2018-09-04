87b877d8d764cd8ba9f898601f30821bb3b2f637
Tony Matthes <tony@Tonys-MacBook-Pro.local>
2018-09-04 14:40:15 -0500

    buzzer beater! got the swap function working at the last moment, didn't get to the delete function, but I'm confident in my ability to do that

--- client.js
+++ client.js
@@ -2,17 +2,39 @@ let counter = 0
 
 $(document).ready(onReady);
 
-function onReady(){
+function onReady() {
     $('#createButton').on('click', addDiv);
+    $(document).on('click', '.swapButton', swapColor);
+    // $('.').on('click', '.deleteButton', deleteBox)
+
 }
 
-function addDiv(){
+function addDiv() {
     counter++;
     console.log(counter);
     $('body').append(`
-    <div class="colorBoxRed">
+    <div class=" Red">
         <p>${counter}</p>
         <button class="swapButton">Swap</button>
         <button class="deleteButton">Delete</button>
     </div`);
 }
+
+function swapColor() {
+    let container = $(this).closest('div');
+    console.log(container)
+    if (container.attr('class') == 'Red' ){
+        container.toggleClass('Yellow');
+        container.toggleClass('Red');
+    }
+    else{
+        container.toggleClass('Yellow', false);   
+        container.toggleClass('Red', true);
+           
+    }
+}
+
+// function deleteBox(){
+    // i know i know how to do this, just didn't get to it after figuring out the swap
+
+// }
